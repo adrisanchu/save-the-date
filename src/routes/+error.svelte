dce1a1423ff56af01e4b1b40e968051b506f0de0
adrisanchu <adrian.sanchez@constant-urban.eu>
2024-01-05 12:12:05 +0100

    use `$app/paths` to get the base path in both dev and prod (from `process.env`)

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	console.log('url', $page.url);
-	const homepageUrl = $page.url.origin && '/' && process.env.BASE_PATH;
-	console.log('home', homepageUrl);
+	import { base } from '$app/paths';
+	console.log('url:', $page.url);
+	console.log('base url:', base);
 </script>
 
 <div class="card p-4">
@@ -11,6 +11,6 @@
 	</header>
 	<blockquote>The page you are looking for cannot be found</blockquote>
 	<footer class="card-footer mt-4 p-0">
-		<a href={homepageUrl}>Return to homepage</a>
+		<a href="/{base}">Return to homepage</a>
 	</footer>
 </div>
