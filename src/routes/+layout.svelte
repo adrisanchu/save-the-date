0b094f6c0a82af8e066371ee95809c5c150e9d52
adrisanchu <adrian.sanchez@ree.es>
2024-02-09 18:21:05 +0100

    set image as background on the landing page (wip)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,18 @@
 </script>
 
 <PageHeader />
-<div class="flex flex-col justify-center items-center min-h-screen">
+<div id="main" class="flex flex-col justify-center items-center min-h-screen">
 	<slot />
 </div>
+
+<style>
+	#main {
+		height: 100vh;
+		background-image: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
+			url('/img/img_2.jpeg');
+		background-size: cover;
+		background-position: center;
+		text-shadow: 0 0.05rem 0.1rem rgba(0, 0, 0, 0.5);
+		box-shadow: inset 0 0 5rem rgba(0, 0, 0, 0.5);
+	}
+</style>
