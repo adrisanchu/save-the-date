e6129dcd1d97dfb2b32bd6b7629950f2e1a73abf
adrisanchu <adrian.sanchez@ree.es>
2024-02-10 19:49:16 +0100

    init transport page + fix error layout + `npm audit fix`

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,18 +2,20 @@
 	import { page } from '$app/stores';
 	import { base } from '$app/paths';
 
-	const baseUrl = base || "/";
+	const baseUrl = base || '/';
 
 	console.log('url:', $page.url);
 	console.log('base url:', baseUrl);
 </script>
 
-<div class="card p-4">
-	<header class="card-header text-center mb-4 p-0">
-		<h2>{$page.status}: {$page.error?.message}</h2>
-	</header>
-	<blockquote>The page you are looking for cannot be found</blockquote>
-	<footer class="card-footer mt-4 p-0">
-		<a href={baseUrl}>Return to homepage</a>
-	</footer>
+<div class="flex flex-col justify-center items-center min-h-screen">
+	<div class="card p-4">
+		<header class="card-header text-center mb-4 p-0">
+			<h2>{$page.status}: {$page.error?.message}</h2>
+		</header>
+		<blockquote>The page you are looking for cannot be found</blockquote>
+		<footer class="card-footer mt-4 p-0">
+			<a href={baseUrl}>Return to homepage</a>
+		</footer>
+	</div>
 </div>
---
+++ src/routes/transport/+page.svelte
@@ -0,0 +1,20 @@
+<h2 id="transport" class="ml-6 h2 pt-16 mb-4">Transporte</h2>
+<div class="flex flex-col justify-center items-center">
+	<div class="container card p-4">
+		<p>Se contará con servicio de autobuses:</p>
+		<ul class="list mt-4">
+			<li>
+				<span class="badge bg-primary-500">13h</span>
+				<span class="flex-auto">
+					Autobús de ida desde la Iglesia hasta Can Ribas al acabar la ceremonia
+				</span>
+			</li>
+			<li>
+				<span class="badge bg-primary-500">23h</span>
+				<span class="flex-auto">
+					Autobús de vuelta al acabar la fiesta de Can Ribas hasta Barcelona
+				</span>
+			</li>
+		</ul>
+	</div>
+</div>
