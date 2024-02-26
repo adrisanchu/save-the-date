25a378ed9e5f03125f0796812476e7fe6eaf9a9b
adrisanchu <adrian.sanchez@ree.es>
2024-02-26 20:57:25 +0100

    there was a missing anchor link to `/` without the sveltekit base path...making github deploy to fail

--- src/lib/components/HeroIcon.svelte
+++ src/lib/components/HeroIcon.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
+	import { base } from '$app/paths';
 	export let currentUrl: string = '/';
+	const baseUrl = base || '/';
 </script>
 
 <a
 	class="h5"
 	aria-current={currentUrl === '/' ? 'page' : undefined}
-	href="/"
+	href="{baseUrl}"
 >
 	A & I
 </a>
