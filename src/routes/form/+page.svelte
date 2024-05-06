f8c6ee99f9614e49eaa3ac0325810f07eb6711f2
adrisanchu <adrian.sanchez@ree.es>
2024-05-06 14:29:33 +0200

    allergies added + data type matching firestore schema + createdAt timestamp from firestore server

--- src/routes/form/+page.svelte
+++ src/routes/form/+page.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
 	import db from '$lib/db/firebase';
-	import { collection, addDoc } from 'firebase/firestore';
+	import { collection, addDoc, updateDoc, serverTimestamp } from 'firebase/firestore';
 
 	let form: any = {
 		missing: false
+	};
+
+	type Survey = {
+		name: string;
+		surname: string;
+		email: string;
+		assistance: boolean;
+		allergies?: string[];
+		otherAllergies?: string;
 	};
 
 	async function handleSubmit(event: { currentTarget: EventTarget & HTMLFormElement }) {
@@ -13,24 +22,93 @@
 		form.email = data.get('email');
 		// console.log('data:', { name: form.name, surname: form.surname, email: form.email });
 
-		// check for mandatory props
+		// Check for mandatory props
 		if (!form.name || !form.surname) {
 			console.error('Missing fields!');
 			form.missing = true;
 			return;
 		}
 
+		// Build Survey Doc to be stored
+		const surveyDoc: Survey = {
+			name: form.name,
+			surname: form.surname,
+			email: form.email,
+			assistance: assistance
+		};
+
+		// Keep only selected allergies
+		const selectedAllergies = allergies.filter((allergy) => allergy.checked);
+		const allergiesStored = selectedAllergies.map((allergy) => {
+			return allergy.accessor;
+		});
+
+		// Add allergies to Survey Doc (if any)
+		if (allergiesStored.length > 0) surveyDoc.allergies = allergiesStored;
+
+		// If other allergies...
+		if (allergiesStored.includes('other')) {
+			let otherAllergy: Allergy | undefined = allergies.filter(
+				(allergy: Allergy) => allergy.accessor === 'other'
+			)[0];
+			surveyDoc.otherAllergies = otherAllergy.value;
+		}
+
+		// console.log('survey doc:', surveyDoc);
+
 		// Store into DB
 		try {
-			const docRef = await addDoc(collection(db, 'surveys'), {
-				name: form.name,
-				surname: form.surname,
-				email: form.email
-			});
+			const docRef = await addDoc(collection(db, 'surveys'), surveyDoc);
 			console.log('Document written with ID: ', docRef.id);
+			// Update the timestamp field with the value from the server
+			await updateDoc(docRef, {
+				createdAt: serverTimestamp()
+			});
 		} catch (e) {
 			console.error('Error adding document: ', e);
 		}
+	}
+
+	let assistance: boolean = true;
+
+	type Allergy = {
+		accessor: string;
+		name: string;
+		checked: boolean;
+		value?: string;
+	};
+
+	let allergies: Allergy[] = [
+		{
+			accessor: 'gluten',
+			name: 'Gluten/celíaco',
+			checked: false
+		},
+		{
+			accessor: 'fish',
+			name: 'Pescado',
+			checked: false
+		},
+		{
+			accessor: 'seafood',
+			name: 'Marisco',
+			checked: false
+		},
+		{
+			accessor: 'other',
+			name: 'Otros (especificar a continuación)',
+			checked: false,
+			value: ''
+		}
+	];
+
+	function handleAllergyCheck(id: string) {
+		allergies = allergies.map((allergy) => {
+			if (allergy.accessor === id) {
+				allergy.checked = !allergy.checked;
+			}
+			return allergy;
+		});
 	}
 </script>
 
@@ -40,7 +118,7 @@
 		<form
 			method="POST"
 			on:submit|preventDefault={handleSubmit}
-			class="container card p-4 mx-4 mb-2 space-y-2"
+			class="container card p-4 mx-4 mb-2 space-y-4"
 		>
 			<!-- name -->
 			<label for="name" class="label">
@@ -82,9 +160,64 @@
 					autocomplete="email"
 				/>
 			</label>
+			<!-- confirm -->
+			<label for="confirm" class="label">
+				<span>¿Vas a venir a la boda?</span>
+				<div class="space-y-2">
+					<label class="flex items-center space-x-2">
+						<input
+							class="radio"
+							type="radio"
+							name="radio-direct"
+							value="true"
+							checked={assistance}
+							on:click={() => (assistance = true)}
+						/>
+						<p>Sí</p>
+					</label>
+					<label class="flex items-center space-x-2">
+						<input
+							class="radio"
+							type="radio"
+							name="radio-direct"
+							value="false"
+							checked={!assistance}
+							on:click={() => (assistance = false)}
+						/>
+						<p>No</p>
+					</label>
+				</div>
+			</label>
+			<!-- alergias ? -->
+			<label for="allergies" class="label">
+				<span>Alérgenos/Intolerancias</span>
+				<div class="space-y-2">
+					{#each allergies as allergy (allergy.accessor)}
+						<label class="flex items-center space-x-2">
+							<input
+								class="checkbox"
+								type="checkbox"
+								checked={allergy.checked}
+								on:click={() => handleAllergyCheck(allergy.accessor)}
+							/>
+							<p>{allergy.name}</p>
+						</label>
+						{#if allergy.accessor == 'other' && allergy.checked}
+							<label class="label">
+								<textarea
+									class="textarea"
+									rows="4"
+									placeholder="Detalla otras alergias/intolerancias..."
+									bind:value={allergy.value}
+								/>
+							</label>
+						{/if}
+					{/each}
+				</div>
+			</label>
 
 			<div class="pt-4 flex justify-end">
-				<button class=" btn variant-filled" type="submit">Submit</button>
+				<button class=" btn variant-filled" type="submit">Enviar</button>
 			</div>
 		</form>
 	</div>
