4b187e3c3cb66df1d01bfdc332e392fa5bb0ce39
adrisanchu <adrian.sanchez@ree.es>
2024-05-15 12:01:22 +0200

    adding multiple invites per form submitted (WIP) + todo.md added

--- src/routes/form/+page.svelte
+++ src/routes/form/+page.svelte
@@ -13,6 +13,7 @@
 		assistance: boolean;
 		allergies?: string[];
 		otherAllergies?: string;
+		invites?: Invite[];
 	};
 
 	async function handleSubmit(event: { currentTarget: EventTarget & HTMLFormElement }) {
@@ -29,6 +30,15 @@
 			return;
 		}
 
+		// If invites...
+		console.log(invites);
+		console.log(invites.some((invite) => !invite.name));
+		if (invites.length > 0 && invites.some((invite) => !invite.name)) {
+			console.error('Missing invite names!');
+			form.missing = true;
+			return;
+		}
+
 		// Build Survey Doc to be stored
 		const surveyDoc: Survey = {
 			name: form.name,
@@ -36,6 +46,9 @@
 			email: form.email,
 			assistance: assistance
 		};
+
+		// If invites...
+		surveyDoc.invites = invites;
 
 		// Keep only selected allergies
 		const selectedAllergies = allergies.filter((allergy) => allergy.checked);
@@ -54,9 +67,10 @@
 			surveyDoc.otherAllergies = otherAllergy.value;
 		}
 
-		// console.log('survey doc:', surveyDoc);
+		console.log('survey doc:', surveyDoc);
 
 		// Store into DB
+		/*
 		try {
 			const docRef = await addDoc(collection(db, 'surveys'), {
 				...surveyDoc,
@@ -66,6 +80,7 @@
 		} catch (e) {
 			console.error('Error adding document: ', e);
 		}
+		*/
 	}
 
 	let assistance: boolean = true;
@@ -108,6 +123,36 @@
 			}
 			return allergy;
 		});
+	}
+
+	type Invite = {
+		id: string;
+		type: string; // couple, child, other
+		age?: number; // for children
+		name: string;
+	};
+
+	let invites: Invite[] = [];
+
+	function newInvite() {
+		const uuid = crypto.randomUUID();
+		console.log('add invite: ', uuid);
+		invites = [
+			...invites,
+			{
+				id: uuid,
+				type: 'couple',
+				name: ''
+			}
+		];
+	}
+
+	function removeInvite(id: string) {
+		console.log('remove invite: ', id);
+		const index = invites.findIndex((invite) => invite.id === id);
+		if (index !== -1) {
+			invites = [...invites.slice(0, index), ...invites.slice(index + 1)];
+		}
 	}
 </script>
 
@@ -187,36 +232,98 @@
 					</label>
 				</div>
 			</label>
-			<!-- alergias ? -->
-			<label for="allergies" class="label">
-				<span>Alérgenos/Intolerancias</span>
-				<div class="space-y-2">
-					{#each allergies as allergy (allergy.accessor)}
-						<label class="flex items-center space-x-2">
-							<input
-								class="checkbox"
-								type="checkbox"
-								checked={allergy.checked}
-								on:click={() => handleAllergyCheck(allergy.accessor)}
-							/>
-							<p>{allergy.name}</p>
-						</label>
-						{#if allergy.accessor == 'other' && allergy.checked}
-							<label class="label">
-								<textarea
-									class="textarea"
-									rows="4"
-									placeholder="Detalla otras alergias/intolerancias..."
-									bind:value={allergy.value}
+			{#if assistance}
+				<!-- +X -->
+				<label for="confirm" class="label">
+					<span>¿Con quién?</span>
+					<button
+						type="button"
+						class="ml-2 font-bold btn-icon btn-icon-sm variant-filled"
+						on:click={newInvite}
+					>
+						+
+					</button>
+					<div class="space-y-2">
+						{#each invites as invite, idx (invite.id)}
+							<div class="card p-4">
+								<header class="flex items-center justify-between mb-2">
+									<span class="font-bold">Acompañante {idx + 1}:</span>
+									<button
+										type="button"
+										class="w-8 h-8 font-bold btn-icon btn-icon-sm variant-filled"
+										on:click={() => removeInvite(invite.id)}>X</button
+									>
+								</header>
+								<section class="max-w-lg">
+									<div class="flex space-x-2">
+										<select class="select" bind:value={invite.type}>
+											<option value="couple">Pareja</option>
+											<option value="child">Niño/a</option>
+											<option value="other">Otro</option>
+										</select>
+										{#if invite.type === 'child'}
+											<input
+												class="input w-24"
+												name="age"
+												type="number"
+												placeholder="Edad"
+												bind:value={invite.age}
+											/>
+										{:else}
+											<div class="w-36"></div>
+										{/if}
+									</div>
+									<!-- name -->
+									<label for="name" class="label">
+										<span>Nombre *</span>
+										<input
+											name="name"
+											class="input"
+											class:input-error={form?.missing && !invite.name}
+											type="text"
+											placeholder="Juan"
+											bind:value={invite.name}
+										/>
+										{#if form?.missing && !invite.name}
+											<p class="text-xs text-error-500">Nombre obligatorio</p>
+										{/if}
+									</label>
+								</section>
+							</div>
+						{/each}
+					</div>
+				</label>
+				<!-- alergias ? -->
+				<label for="allergies" class="label">
+					<span>Alérgenos/Intolerancias</span>
+					<div class="space-y-2">
+						{#each allergies as allergy (allergy.accessor)}
+							<label class="flex items-center space-x-2">
+								<input
+									class="checkbox"
+									type="checkbox"
+									checked={allergy.checked}
+									on:click={() => handleAllergyCheck(allergy.accessor)}
 								/>
+								<p>{allergy.name}</p>
 							</label>
-						{/if}
-					{/each}
-				</div>
-			</label>
+							{#if allergy.accessor == 'other' && allergy.checked}
+								<label class="label">
+									<textarea
+										class="textarea"
+										rows="4"
+										placeholder="Detalla otras alergias/intolerancias..."
+										bind:value={allergy.value}
+									/>
+								</label>
+							{/if}
+						{/each}
+					</div>
+				</label>
+			{/if}
 
 			<div class="pt-4 flex justify-end">
-				<button class=" btn variant-filled" type="submit">Enviar</button>
+				<button class="btn variant-filled" type="submit">Enviar</button>
 			</div>
 		</form>
 	</div>
---
+++ todo.md
@@ -0,0 +1,25 @@
+Tareas:
+
+- [x] quitar botón Inicio
+- [x] añadir link a hero icon (A&I)
+- [ ] añadir secciones:
+
+  - [x] Itinerario: agenda (mapas Google Maps) + transporte
+  - [ ] Recomendaciones: Hoteles, Bares, etc...
+  - [ ] Sección FAQ?
+  - [ ] Contacto: Luna de miel + IBAN + teléfonos
+
+- [ ] Formulario confirmación de asistencia:
+
+  - [x] Básico: datos personales + alergias + asistencia...
+  - [x] Setup con Firebase
+  - [ ] Añadir listado de acompañantes (varios para familias?)
+  - [ ] Añadir confirmación bus ida, vuelta o ambos
+  - [ ] Avanzado: on:click => loading... y al acabar, sacar pop-up de confirmación!
+
+- [ ] Repensar navegación a single page? estilo la de Vantage?
+      (seguramente haya que insertar un e.preventDefault para evitar añadir los # a la url...)
+
+- [ ] Probar a modificar el tema para que el texto vaya en el tono de las invitaciones
+- [ ] Letra de la web? Quicksand? La de las invits?
+- [ ] Añadir image meta tag para que salga al compartir el enlace
