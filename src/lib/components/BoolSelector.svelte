b69cf099b48115a36d50195dfe4986ae70d7a9fd
adrisanchu <adrian.sanchez@constant-urban.eu>
2024-05-16 12:33:17 +0200

    components: create a select component to reuse it across the form

---
+++ src/lib/components/BoolSelector.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+
+	// name is used to define a specific name for the whole group set of options
+	// all input options must share the same name!
+	export let name: string | undefined = crypto.randomUUID();
+	export let value: boolean;
+	export let label: string | undefined;
+	export let yesLabel: string = 'Yes';
+	export let noLabel: string = 'No';
+</script>
+
+<label for="confirm" class="label">
+	{#if label}
+		<span>{label}</span>
+	{/if}
+	<div class="flex space-x-4">
+		<label class="flex items-center space-x-2">
+			<input
+				class="radio"
+				type="radio"
+				{name}
+				value="true"
+				checked={value}
+				on:click={() => dispatch('true')}
+			/>
+			<p>{yesLabel}</p>
+		</label>
+		<label class="flex items-center space-x-2">
+			<input
+				class="radio"
+				type="radio"
+				{name}
+				value="false"
+				checked={!value}
+				on:click={() => dispatch('false')}
+			/>
+			<p>{noLabel}</p>
+		</label>
+	</div>
+</label>
