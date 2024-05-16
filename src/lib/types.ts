436933eaac9ab443ad7987f9e69d9112655486d1
adrisanchu <adrian.sanchez@constant-urban.eu>
2024-05-16 13:11:31 +0200

    BREAKING: changes in types towards a cleaner backend implementation!

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,11 +1,39 @@
-export type Invite = {
+type User = {
+	id: string;
+	name: string;
+	surname: string;
+	email?: string;
+};
+
+export type Invite = User & {
+	type: string; // main, couple, child, other
+	age?: number; // for children
+	allergies?: string[]; // list of strings matching allergy accessors
+	otherAllergies?: string;
+};
+
+export type Survey = {
+	id?: string; // firebase id
+	createdAt: string; // ISO date string
+	createdBy: User; // the user who filled the form
+	assistance: boolean; // common for all invites within the survey
+};
+
+// If assistance is true...
+export type ConfirmedSurvey = Survey & {
+	invites: Invite[]; // at least one: the main invite (the one filling the form!)
+	busGo: boolean; // common for all invites
+	busReturn: boolean; // common for all invites
+};
+
+export type InviteOld = {
 	id: string;
 	type: string; // couple, child, other
 	age?: number; // for children
 	name: string;
 	surname?: string;
 	allergies?: string[]; // list of strings matching allergy accessors
-  otherAllergies?: string;
+	otherAllergies?: string;
 };
 
 export type Allergy = {
@@ -14,7 +42,7 @@ export type Allergy = {
 	checked: boolean;
 };
 
-export type Survey = {
+export type SurveyOld = {
 	name: string;
 	surname: string;
 	email: string;
