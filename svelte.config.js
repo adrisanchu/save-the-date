617782b944b1499168f74db50c3a4723b16896a7
adrisanchu <adrian.sanchez@constant-urban.eu>
2024-08-15 11:46:25 +0200

    please gh-pages stop trying to render a dynamic endpoint!

--- svelte.config.js
+++ svelte.config.js
@@ -17,15 +17,16 @@ const config = {
 			assets: 'build',
 			fallback: '404.html',
 			precompress: false,
-			strict: true
+			strict: false
 		}),
 		paths: {
+			assets: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		},
 		prerender: {
 			// use relative URLs similar to an anchor tag <a href="/test/1"></a>
 			// do not include group layout folders in the path such as /(group)/test/1
-			entries: ['/form', '/form/1']
+			// entries: ['/form', '/form/1']
 		}
 	}
 };
