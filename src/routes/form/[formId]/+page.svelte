bbed1d7be62e1aff59dedbdb05fd3715968def4f
adrisanchu <adrian.sanchez@constant-urban.eu>
2024-08-15 11:37:08 +0200

    still not fixing deploy to gh-pages...

--- src/routes/form/[formId]/+page.svelte
+++ src/routes/form/[formId]/+page.svelte
@@ -1,10 +1,21 @@
-<script lang="ts">
+<script>
+	import { onMount } from 'svelte';
 	import { page } from '$app/stores';
+
+	let data = {
+		id: ''
+	};
+
+	onMount(() => {
+		data = {
+			id: $page.params.formId
+		};
+	});
 </script>
 
 <div class="flex flex-col">
 	<h2 id="form" class="h2 mb-4 ml-6 pt-20">
 		Formulario de Asistencia
-		<span class="variant-filled chip">{$page.params.formId}</span>
+		<span class="variant-filled chip">{data.id}</span>
 	</h2>
 </div>
