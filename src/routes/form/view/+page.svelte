f9de769c6482b34aec7b34f62afe8123a70c9bce
adrisanchu <adrian.sanchez@constant-urban.eu>
2024-08-15 17:56:45 +0200

    start refactoring to see if it works now from gh-pages

--- src/routes/form/view/+page.svelte
+++ src/routes/form/view/+page.svelte
@@ -1,21 +1,102 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { page } from '$app/stores';
+	import db from '$lib/db/firebase';
+	import { doc, getDoc } from 'firebase/firestore';
+	import type { Survey, Invite } from '$lib/types';
+	import InviteCardSubmitted from '$lib/components/InviteCardSubmitted.svelte';
 
-	let id: string | null = null;
+	let status: string = 'pending';
+	let survey: Survey;
+	let mainInvite: Invite;
+	let invites: Invite[] = [];
+
+	const getInvite = async (id: string) => {
+		const docSnap = await getDoc(doc(db, 'invites', id));
+		if (docSnap.exists()) {
+			return docSnap.data() as Invite;
+		} else {
+			// docSnap.data() will be undefined in this case
+			console.error('No invite found for id: ', id);
+			throw new Error(`The document with id: "${id}" does not exist!`);
+		}
+	};
+
+	const getSurvey = async () => {
+		const formId = new URLSearchParams(window.location.search).get('id') || '';
+		const docRef = doc(db, 'surveys', formId);
+		const docSnap = await getDoc(docRef);
+
+		if (docSnap.exists()) {
+			// add id to firestore object
+			return { ...docSnap.data(), id: formId } as Survey;
+		} else {
+			// docSnap.data() will be undefined in this case
+			throw new Error(`The document with id: "${formId}" does not exist!`);
+		}
+	};
+
+	const sleep = (delay: number) => new Promise((resolve) => setTimeout(resolve, delay));
+	const sleepTrigger = async () => {
+		await sleep(5000);
+	};
+
+	const fetchData = async () => {
+		sleepTrigger().then(() => {
+			getSurvey()
+				.then((data) => {
+					console.log('Promise resolved!');
+					console.log('Data: ', data);
+					survey = data;
+
+					if (survey.invites.length > 0) {
+						// populate invites array
+						survey.invites.forEach((id) => {
+							getInvite(id).then((inv) => {
+								if (inv.type == 'main') {
+									mainInvite = inv;
+									console.log('main invite data: ', mainInvite);
+								} else {
+									invites = [...invites, inv];
+								}
+							});
+						});
+					}
+					status = 'success';
+					console.log('status: ', status);
+				})
+				.catch((err) => {
+					console.error('Err: ', err);
+					status = 'error';
+				});
+		});
+	};
 
 	onMount(() => {
-		id = $page.url.searchParams.get('id');
+		console.log('Client ready');
+		console.log('status: ', status);
+
+		fetchData();
 	});
 </script>
 
 <div class="flex flex-col">
-	{#if id}
+	{#if status == 'error'}
+		<h2 id="form" class="h2 mb-4 ml-6 pt-20">Formulario no encontrado :(</h2>
+	{:else if status == 'pending'}
+		<h2 id="form" class="h2 mb-4 ml-6 pt-20">Cargando...</h2>
+	{:else if survey}
 		<h2 id="form" class="h2 mb-4 ml-6 pt-20">
 			Formulario de Asistencia
-			<span class="variant-filled chip">{id}</span>
+			<span class="variant-filled chip">{survey.id}</span>
 		</h2>
-	{:else}
-		<h2 id="form" class="h2 mb-4 ml-6 pt-20">Identificador no encontrado</h2>
+
+		<div class="flex flex-wrap items-center justify-center">
+			{#if mainInvite}
+				<div class="card container mx-4 mb-2 space-y-4 p-4">
+					<h3 class="h3">Datos personales:</h3>
+					<InviteCardSubmitted invite={mainInvite} />
+				</div>
+			{/if}
+		</div>
 	{/if}
 </div>
