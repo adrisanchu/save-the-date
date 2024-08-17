55d2eb8100c9bdb02f4aabf8caa9a82f74bd419e
adrisanchu <adrian.sanchez@constant-urban.eu>
2024-08-17 18:53:33 +0200

    new placeholder component based on skeleton (https://www.skeleton.dev/elements/placeholders)

---
+++ src/lib/components/Placeholder.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let animated: boolean = false;
+	let cssClass: string = animated ? 'placeholder animate-pulse' : 'placeholder';
+</script>
+
+<div class="space-y-4 p-4">
+	<div class={cssClass} />
+	<div class="grid grid-cols-3 gap-8">
+		<div class={cssClass} />
+		<div class={cssClass} />
+		<div class={cssClass} />
+	</div>
+	<div class="grid grid-cols-4 gap-4">
+		<div class={cssClass} />
+		<div class={cssClass} />
+		<div class={cssClass} />
+		<div class={cssClass} />
+	</div>
+</div>
