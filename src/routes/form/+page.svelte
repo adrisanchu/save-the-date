83c0013ca6b119b4324d2f6087d6791cfcad2d24
adrisanchu <adrian.sanchez@constant-urban.eu>
2024-10-02 21:26:02 +0200

    improved form landing panel and check network when fetching data from database. Display error msgs

--- src/routes/form/+page.svelte
+++ src/routes/form/+page.svelte
@@ -12,6 +12,19 @@
 	let searchCode: string = '';
 	const localSurveys: Survey[] = [];
 
+	const searchStatus = {
+		missing: false,
+		error: false,
+		networkError: false
+	};
+
+	$: if (!searchCode) {
+		// reset default values
+		searchStatus.missing = false;
+		searchStatus.error = false;
+		searchStatus.networkError = false;
+	}
+
 	/**
 	 * Find a survey based on the input field
 	 */
@@ -19,14 +32,24 @@
 		if (searchCode) {
 			// find the code on the server
 			const docRef = doc(db, 'surveys', searchCode);
-			const docSnap = await getDoc(docRef);
-			if (docSnap.exists()) {
-				// navigate to the page
-				goto(`${base}/form/view?id=${searchCode}`);
-			} else {
-				console.error('No such document!');
-				// TODO: display an error
+			try {
+				const docSnap = await getDoc(docRef);
+				if (docSnap.exists()) {
+					// navigate to the page
+					goto(`${base}/form/view?id=${searchCode}`);
+				} else {
+					console.error('No such document!');
+					// TODO: display an error
+					searchStatus.error = true;
+				}
+			} catch (err) {
+				searchStatus.networkError = true;
+				console.error('Error fetching user data:', err);
+				return;
 			}
+		} else {
+			// code is missing!
+			searchStatus.missing = true;
 		}
 	}
 
@@ -51,18 +74,31 @@
 
 <div class="flex flex-col">
 	<h2 id="form" class="h2 mb-4 ml-6 pt-20">Confirmación de asistencia</h2>
-	<div class="mx-4 flex flex-col justify-center space-y-6 md:mx-auto">
+	<div class="mx-4 flex flex-col justify-center md:mx-auto">
 		<a class="variant-filled btn ml-2 font-bold" href="{base}/form/new">Nuevo Formulario</a>
-		<div class="separator"><span class="text-surface-900">O bien...</span></div>
+		<div class="separator my-4"><span class="text-surface-900">O bien...</span></div>
+		<p class="mb-2 flex justify-center">Consulta tu formulario (si ya lo rellenaste)</p>
 		<div class="input-group input-group-divider grid-cols-4">
 			<input
 				type="search"
 				class="input col-span-3"
+				class:input-error={(!searchCode && searchStatus?.missing) ||
+					searchStatus?.error ||
+					searchStatus?.networkError}
 				placeholder="Código de formulario"
 				bind:value={searchCode}
 			/>
 			<button type="button" class="variant-filled font-bold" on:click={findSurvey}>Buscar</button>
 		</div>
+		{#if searchStatus?.error && searchCode}
+			<p class="mx-3 mt-2 text-xs text-error-500">Código no encontrado!</p>
+		{/if}
+		{#if searchStatus?.networkError && searchCode}
+			<p class="mx-3 mt-2 text-xs text-error-500">Error de red: Comprueba tu conexión a Internet</p>
+		{/if}
+		{#if searchStatus?.missing && !searchCode}
+			<p class="mx-3 mt-2 text-xs text-error-500">Introduce un código que buscar!</p>
+		{/if}
 	</div>
 	<div class="mx-4 mt-12 md:mx-auto">
 		{#if $surveys.length > 0}
